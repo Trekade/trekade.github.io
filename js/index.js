7a14e6cd8a893aa549e0c924f5049752dc7dcbbb
Daniel Schmitz <daniel@bashgeek.net>
2019-07-24 00:32:40 +0800

    JS Fix

--- js/index.js
+++ js/index.js
@@ -1,13 +1,4 @@
-/*
-Theme Name: HUGE
-Description: Creative Coming Soon Template
-Author: SquirrelLabs
-Author URI: https://themeforest.net/user/squirrellabs/portfolio?ref=SquirrelLab
-Version: 1.0
-License: https://themeforest.net/licenses/standard
-*/
-
-$(window).on("load",function(){$("#countdown").countdown($("#countdown").attr("data-time"),function(a){$(this).html(a.strftime("<div>%D<span>Days</span></div> <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>"))})});var camera,controls,scene,renderer,mouseX=0,mouseY=0;scene=new THREE.Scene;scene.fog=new THREE.FogExp2(401713,.002);renderer=new THREE.WebGLRenderer;renderer.setClearColor(scene.fog.color);renderer.setPixelRatio(window.devicePixelRatio);
+var camera,controls,scene,renderer,mouseX=0,mouseY=0;scene=new THREE.Scene;scene.fog=new THREE.FogExp2(401713,.002);renderer=new THREE.WebGLRenderer;renderer.setClearColor(scene.fog.color);renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth,window.innerHeight);var container=document.getElementById("triangles");container.appendChild(renderer.domElement);camera=new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,1,3E3);camera.position.z=600;var geometry=new THREE.CylinderGeometry(0,10,15,3,1),materials=[new THREE.MeshLambertMaterial({color:2701106}),new THREE.MeshLambertMaterial({color:668489}),new THREE.MeshLambertMaterial({color:1780033})],tris=[],pivot=new THREE.Object3D;
 pivot.position=scene.position;for(var i=0;500>i;i++)tris[i]=new THREE.Mesh(geometry,materials[getRandom(0,2)]),tris[i].position.x=1E3*(Math.random()-.5),tris[i].position.y=1E3*(Math.random()-.5),tris[i].position.z=1E3*(Math.random()-.5),tris[i].rotation.x=1E3*(Math.random()-.5),tris[i].rotation.y=1E3*(Math.random()-.5),tris[i].o_pos_x=tris[i].position.x,tris[i].o_pos_y=tris[i].position.y,pivot.add(tris[i]);scene.add(pivot);
 var size=200,point,outerGeo=new THREE.CylinderGeometry(size,size,20,3,20),innerGeo=new THREE.CylinderGeometry(size-5,size-5,20,3,20),outerBSP=new ThreeBSP(outerGeo),innerBSP=new ThreeBSP(innerGeo),intersections=outerBSP.subtract(innerBSP),mainMat=new THREE.MeshBasicMaterial({color:16777215,vertexColors:THREE.FaceColors}),faceIndices=["a","b","c"],mainGeo=intersections.toGeometry();
